195828ed4449463eaeed6e2aaf364755d74ab13b
ragnarlodbrok1992 <maraus92@gmail.com>
2018-01-25 20:55:07 +0100

    Grow.exe execution logic

--- cygwin_test.py
+++ cygwin_test.py
@@ -1,7 +1,6 @@
 import os
 from pprint import pprint
 import subprocess
-from subprocess import Popen, PIPE
 
 if __name__ == '__main__':
     cygwin_bin = "C:\\cygwin64\\bin\\"
@@ -10,35 +9,19 @@ if __name__ == '__main__':
     cygwin_mintty = "C:\\cygwin64\\bin\\mintty.exe"
     cygwin_bash = "C:\\cygwin64\\bin\\bash.exe"
 
-    # print(os.getcwd())
-    # print(os.path.abspath(cygwin_path))
-    # pprint(os.listdir(os.path.abspath(cygwin_path)))
-    # pprint(os.system("dir"))
-    # pprint(os.system(cygwin_bin + " --help"))
-    # pprint(os.system(cygwin_bin + " /bin/bash"))
-    # p = Popen(cygwin_bash, stdin=PIPE, stdout=PIPE)
-    # p.stdin.write("echo $PWD")
-    # p.stdin.write("echo $PATH")
-    # p = Popen("cmd", stdin=PIPE, stdout=PIPE)
-    #
-    # p.stdin.write("dir")
-    # p.stdin.close()
-    # out = p.stdout.read()
-    # pprint(out)
-    # output = subprocess.check_output(['cmd', 'dir'])
-    # output = os.system('cmd dir')
-    # output = subprocess.call('', shell=True)
-    # output = subprocess.call(cygwin_bin + "gcc.exe", shell=True)
     current_dir = os.path.dirname(__file__)
-    p = subprocess.Popen(current_dir + "/Sleuth3/grow.exe")
-    # p.communicate()
-    # out = p.stdout.read()
+    grow_dir = current_dir + "/Sleuth3/"
+    grow_exe_dir = grow_dir + "grow.exe"
+    scenario_dir = grow_dir + "Scenarios/"
+    pprint(grow_dir)
+    pprint(scenario_dir)
+    pprint(os.getcwd())
+    pprint(os.path.exists(scenario_dir))
+    os.chdir(scenario_dir)
+    p = subprocess.Popen("../grow.exe calibrate scenario.gdansk_fun1")
     p.wait()
-    # pprint(p.stdout.read())
     out, err = p.communicate()
     return_code = p.returncode
-    pprint(current_dir + "/Sleuth3/grow.exe")
     pprint(out)
     pprint(err)
     pprint(return_code)
-    pass
