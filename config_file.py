f10fb2f92c331e451d4a8ada1e9730ba6c843726
ragnarlodbrok1992 <maraus92@gmail.com>
2018-01-25 17:33:13 +0100

    Connecting execution to UI - WIP

---
+++ config_file.py
@@ -0,0 +1,8 @@
+CYGWIN_BIN = "C:\\cygwin64\\bin\\"
+MAKE = " make"
+MAKE_CLEAN = " make clean"
+SLEUTH3 = "\\Sleuth3\\"
+SLEUTHGA = "\\SleuthGeneticAlgorithm\\"
+
+if __name__ == '__main__':
+    pass
---
+++ defaults.py
@@ -0,0 +1,5 @@
+# Here comes the default variables for Scenario file, all of them need to be asserted
+# before running a simulation
+
+if __name__ == '__main__':
+    pass
