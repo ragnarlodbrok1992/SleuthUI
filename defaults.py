f3ab4163a795c24a3492b4ad544edc4b20686a69
ragnarlodbrok1992 <maraus92@gmail.com>
2018-01-31 21:39:14 +0100

    Creating configuration file with Py data structures

--- defaults.py
+++ defaults.py
@@ -1,5 +1,15 @@
 # Here comes the default variables for Scenario file, all of them need to be asserted
 # before running a simulation
 
+
+class Defaults(object):
+
+    def __init__(self):
+        pass
+
+    def write_conf_file(self):
+        pass
+
+
 if __name__ == '__main__':
     pass
